0aeeb21a6f0b540442a537626e7b3d4638a6c398
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2024-10-14 15:54:25 +0200

    feat : Added memory initialisation to '\0'

--- mini_memory.c
+++ mini_memory.c
@@ -26,5 +26,19 @@ void *mini_calloc(int size_element, int number_element) {
     return NULL;
   }
 
+  // Initialisation of memory to '\0'
+  for (size_t i = 0; i < total_size; i++) {
+    // tab[i] = '\0';
+    *(char *)(allocated_memory + i) = '\0';
+  }
+
+  // Verifies that the initialisation went as planned
+  for (size_t i = 0; i < total_size; i++) {
+    if (*(char *)(allocated_memory + i) != '\0') {
+      printf("Memory check failed at index %zu: value is %d, expected 0\n", i,
+             *(char *)(allocated_memory + i));
+      return NULL;
+    }
+  }
   return allocated_memory;
 }

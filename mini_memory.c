522ecba883d4c12ab79bf16964004e4baae55863
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2024-10-14 23:27:23 +0200

    feat : Added a a linked list to track each allocated memory.
    The structure malloc element, contains all the metadatas regarding the
    new allocated memory. The metadata and datas are handled separately and
    dynamically using sbrk.

--- mini_memory.c
+++ mini_memory.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+typedef struct malloc_element {
+  void *memory_ptr;
+  size_t size;
+  int status; // 0 for free, 1 for used
+  struct malloc_element *next;
+} malloc_element;
+
+malloc_element *malloc_list = NULL; // Contains all malloc element
+
 void *mini_calloc(int size_element, int number_element) {
   size_t total_size = size_element * number_element;
 
@@ -24,6 +33,30 @@ void *mini_calloc(int size_element, int number_element) {
            "bytes.\n",
            total_size, allocated_size);
     return NULL;
+  }
+
+  // Allocation of the space for the metadatas of the new element
+  malloc_element *new_element = sbrk(sizeof(malloc_element));
+  if (new_element == (void *)-1) {
+    perror("sbrk failed to allocate memory for malloc_element");
+    return NULL;
+  }
+
+  // Initialisation of the values according to the data allocatd
+  new_element->memory_ptr = allocated_memory;
+  new_element->size = total_size;
+  new_element->status = 1;
+  new_element->next = NULL;
+
+  // Add the new element to the end of the linked list
+  if (malloc_list == NULL) {
+    malloc_list = new_element;
+  } else {
+    malloc_element *temp = malloc_list;
+    while (temp->next != NULL) {
+      temp = temp->next;
+    }
+    temp->next = new_element;
   }
 
   // Initialisation of memory to '\0'

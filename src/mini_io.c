34489a03c8fe8dadea1042c53e3d51e7f7047145
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2024-11-02 20:59:44 +0100

    feat + fix : Added mini_fread function and fixed MYFILE struct to encapsulate it, added mini_fopen prototype.
    As you can see, i've moved the typedef of MYFILE struct in the
    mini_lib.h this achieve encapsulation as in glibc for the FILE type. It
    essentially tells the end user that there is a MYFILE type, so he can
    use it use function that return MYFILE type, but in the meantime the end
    user can't tell how MYFILE behave or how it is implemented. I then hide
    the implementation to the user but still allow him to use it through a
    set of function, mini_fopen() mini_fread() etc...

--- src/mini_io.c
+++ src/mini_io.c
@@ -1,18 +1,20 @@
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "mini_lib.h"
 
 #define IOBUFFER_SIZE 2048
 
-typedef struct MYFILE {
+struct MYFILE {
   int fd;
   void *buffer_read;
   void *buffer_write;
   int ind_read;
   int ind_write;
-} MYFILE;
+};
 
 MYFILE *mini_fopen(char *file, char mode) {
   MYFILE *myFile = (MYFILE *)mini_calloc(sizeof(MYFILE), 1);
@@ -51,5 +53,49 @@ MYFILE *mini_fopen(char *file, char mode) {
     mini_free(myFile);
     return NULL;
   }
+
   return myFile;
+}
+
+int mini_fread(void *buffer, int size_element, int number_element,
+               MYFILE *file) {
+  if (file == NULL || file->fd < 0 || buffer == NULL || size_element <= 0 ||
+      number_element <= 0) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  if (file->buffer_read == NULL) {
+    file->buffer_read = mini_calloc(1, IOBUFFER_SIZE);
+    if (file->buffer_read == NULL) {
+      return -1;
+    }
+    file->ind_read = 0;
+  }
+
+  int bToRead = size_element * number_element;
+  int bRead = 0;
+  char *src = (char *)file->buffer_read;
+  char *dest = (char *)buffer;
+
+  while (bRead < bToRead) {
+    if (file->ind_read >= IOBUFFER_SIZE || file->ind_read == 0) {
+      int read_result = read(file->fd, file->buffer_read, IOBUFFER_SIZE);
+      if (read_result < 0) {
+        return -1;
+      } else if (read_result == 0) {
+        break;
+      }
+      file->ind_read = 0;
+    }
+
+    if (file->ind_read < IOBUFFER_SIZE) {
+      *dest = src[file->ind_read];
+      dest++;
+      file->ind_read++;
+      bRead++;
+    }
+  }
+
+  return bRead / size_element;
 }
--- src/mini_lib.h
+++ src/mini_lib.h
@@ -14,4 +14,11 @@ void mini_perror(char *message);
 void itoa(int n, char *buffer);
 void mini_exit_printf(void);
 
+// stdio
+typedef struct MYFILE MYFILE;
+
+MYFILE *mini_fopen(char *file, char mode);
+int mini_fread(void *buffer, int size_element, int number_element,
+               MYFILE *file);
+
 #endif // !MINI_LIB.H

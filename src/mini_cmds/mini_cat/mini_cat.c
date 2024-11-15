4b07297364bad835d802573d621b2c43de625952
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2024-11-15 16:03:06 +0100

    fix : Fixed and issue with mini_cat.

--- src/mini_cmds/mini_cat/mini_cat.c
+++ src/mini_cmds/mini_cat/mini_cat.c
@@ -1,32 +1,35 @@
+#include <stdio.h>
+
 #include "../../mini_glibc/mini_lib.h"
 
-#define BUF_SIZE 8192
+#define BUF_SIZE 2048
 
 int main(int argc, char *argv[]) {
 
   if (argc != 2) {
     mini_printf("Usage: cat <filename>\n");
-    return 1;
+    mini_exit(1);
   }
 
   char *filename = argv[1];
-  char buffer[BUF_SIZE];
   MYFILE *file = mini_fopen(filename, 'r');
   if (file == NULL) {
     mini_perror("Error opening the file");
-    return 1;
+    mini_exit(1);
   }
 
-  int bytes_read;
-  while ((bytes_read = mini_fread(buffer, sizeof(char), BUF_SIZE, file)) > 0) {
-    buffer[bytes_read] = '\0';
+  char buffer[BUF_SIZE];
+
+  int bRead;
+  while ((bRead = mini_fread(buffer, sizeof(char), 1024, file)) > 0) {
+    buffer[bRead] = '\0';
     mini_printf(buffer);
   }
 
   if (mini_fclose(file) == -1) {
     mini_perror("Error closing the file");
-    return 1;
+    mini_exit(1);
   }
 
-  return 0;
+  mini_exit(0);
 }

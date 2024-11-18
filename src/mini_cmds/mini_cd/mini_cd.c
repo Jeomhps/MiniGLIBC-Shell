aa30b7fe1a3fa052141a77a575fc1b9c2a286774
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2024-11-18 18:39:34 +0100

    fix : fixed mini_cd.

--- src/mini_cmds/mini_cd/mini_cd.c
+++ src/mini_cmds/mini_cd/mini_cd.c
@@ -3,6 +3,10 @@
 #include "../../mini_glibc/mini_lib.h"
 
 int main(int argc, char *argv[]) {
+  if (argc > 2) {
+    mini_printf("Usage : mini_cd <dir> \n");
+  }
+
   if (chdir(argv[1]) != 0) {
     mini_perror("Error : unable to change directory");
     return -1;
